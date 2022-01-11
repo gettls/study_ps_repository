3f6596279c347cf1670b0c7ec2ac3cb7e48e63ae
gettls <gettls@daum.net>
2022-01-11 15:08:07 +0900

    commit

--- gettls/3주차/시간표 짜기.cpp
+++ gettls/3주차/시간표 짜기.cpp
@@ -10,40 +10,16 @@ using namespace std;
 #pragma warning (disable:4996)
 typedef unsigned long long ull;
 int n, m;
-vector<ull> lecture;
-
+string door;
+vector<string> btn;
 int main() {
+	cin >> n >> m;
 
-	cin >> n;
-	lecture.resize(n);
-
-	for (int i = 0; i < n; i++) {
-		int k;
-		cin >> k;
-		for (int j = 0; j < k; j++) {
-			int x; cin >> x;
-			lecture[i] |= ((ull)1 << x);
-		}
-
-	}
-
-	cin >> m;
+	cin >> door;
 
 	for (int i = 0; i < m; i++) {
-		int p; cin >> p;
-		ull tmp = 0;
-		for (int j = 0; j < p; j++) {
-			int q; cin >> q;
-			tmp |= ((ull)1 << q);
-		}
-
-		int cnt = 0;
-		for (int j = 0; j < n; j++) {
-			if (lecture[j] == (lecture[j] & tmp)) {
-				cnt++;
-			}
-		}
-		cout << cnt << "\n";
+		string str; cin >> str;
+		btn.push_back(str);
 	}
 
 }
